a65932b00de70bd1b1bb6c04fe6294d695492232
Carlos Hernández <diamante.supremo.10000@gmail.com>
2025-12-27 14:17:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const projects = [
         views: "1.2k vistas",
         date: "hace 2 días",
         duration: "Node.js",
-        thumbnail: "Devtube/script.js/Microsoft_To-Do_icon.svg.png55a3&query=To+do+listc7-b908-d5e7c26755a3&query=To+do+list", 
+        thumbnail: "https://share.google/mXr0z3UEVCnSwEl54", 
         url: "https://to-do-list-92jq.onrender.com",
         avatarColor: "#2ecc71"
     },
